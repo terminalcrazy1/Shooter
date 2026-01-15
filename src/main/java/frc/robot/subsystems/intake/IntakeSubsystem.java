0108db1b406ae342ef35e503e4d541d3978c5177
JackcN2 <jack.c.noble2@gmail.com>
2026-01-15 13:34:53 -0500

    TunableNumber fix

--- src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
@@ -2,20 +2,21 @@ package frc.robot.subsystems.intake;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.util.LoggedTunableNumber;
 import org.littletonrobotics.junction.AutoLogOutput;
 import org.littletonrobotics.junction.Logger;
-
 
 public class IntakeSubsystem extends SubsystemBase {
   private final IntakeIO io;
 
   private final IntakeIOInputsAutoLogged inputs = new IntakeIOInputsAutoLogged();
+  private final LoggedTunableNumber kS = new LoggedTunableNumber("Intake/kS", 0.5);
+  private final LoggedTunableNumber kV = new LoggedTunableNumber("Intake/kV", 0.5);
+  private final LoggedTunableNumber kP = new LoggedTunableNumber("Intake/kP", 1);
+  private final LoggedTunableNumber kD = new LoggedTunableNumber("Intake/kD", 0);
 
   public IntakeSubsystem(IntakeIO io) {
-    private final LoggedTunableNumber kS = new LoggedTunableNumber("Intake/kS", 0.5);
-    private final LoggedTunableNumber kV = new LoggedTunableNumber("Intake/kV", 0.5);
-    private final LoggedTunableNumber kP = new LoggedTunableNumber("Intake/kP", 1);
-    private final LoggedTunableNumber kD = new LoggedTunableNumber("Intake/kD", 0);
+
     this.io = io;
   }
 
