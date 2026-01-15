c09c7b596a736dcecad25c2597282d499f42cdcd
JackcN2 <jack.c.noble2@gmail.com>
2026-01-15 12:48:21 -0500

    added loggedtunablenumbers

--- src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
@@ -5,12 +5,17 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.littletonrobotics.junction.AutoLogOutput;
 import org.littletonrobotics.junction.Logger;
 
+
 public class IntakeSubsystem extends SubsystemBase {
   private final IntakeIO io;
 
   private final IntakeIOInputsAutoLogged inputs = new IntakeIOInputsAutoLogged();
 
   public IntakeSubsystem(IntakeIO io) {
+    private final LoggedTunableNumber kS = new LoggedTunableNumber("Intake/kS", 0.5);
+    private final LoggedTunableNumber kV = new LoggedTunableNumber("Intake/kV", 0.5);
+    private final LoggedTunableNumber kP = new LoggedTunableNumber("Intake/kP", 1);
+    private final LoggedTunableNumber kD = new LoggedTunableNumber("Intake/kD", 0);
     this.io = io;
   }
 
